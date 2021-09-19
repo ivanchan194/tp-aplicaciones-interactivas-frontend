bf56f9caf496a6b5cf4083152ac2497f87eb89ea
Ivan Chan <ivanchan.194@gmail.com>
2021-09-19 20:07:19 -0300

    Added basic components and header

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,41 @@
+import React from 'react';
 import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Header from '../Header/Header';
+import Dashboard from '../Dashboard/Dashboard';
+import Login from '../Login/Login';
+import Profile from '../Profile/Profile';
+import useToken from './useToken';
 
 function App() {
-  return <></>;
+    const { token, setToken } = useToken();
+
+    console.log("App component")
+
+    //if(!token) { 
+    //  console.log("Token check")
+    //  return <Login setToken={setToken} />
+    //}
+
+    return (
+        <div className="wrapper">
+
+            <BrowserRouter>
+                <Header />
+                <Switch>
+                    <Route path="/dashboard">
+                        <Dashboard />
+                    </Route>
+                    <Route path="/profile">
+                        <Profile />
+                    </Route>
+                    <Route path="/login">
+                        <Login setToken={setToken} />
+                    </Route>
+                </Switch>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+
+import './Header.css';
+
+export default function Header() {
+    console.log("Header component")
+
+    return (
+        <Toolbar className="header">
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                Hospital UADE
+            </Typography>
+            <Button color="inherit"><Link to="/profile">Perfil</Link></Button>
+            <Button color="inherit"><Link to="/register">Crear Cuenta</Link></Button>
+            <Button color="inherit"><Link to="/login">Iniciar Sesión</Link></Button>
+        </Toolbar>
+    );
+}
